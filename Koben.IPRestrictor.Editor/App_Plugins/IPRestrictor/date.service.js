de33285737a9c98999cc8cd7bbd0a2eba12b5323
Jonty Schmidt <jonty@koben.com.au>
2022-09-05 14:56:28 +1000

    Try re-add IPRestrictor App Plugin

---
+++ Koben.IPRestrictor.Editor/App_Plugins/IPRestrictor/date.service.js
@@ -0,0 +1,15 @@
+angular.module('umbraco.resources').factory('ipRestrictorDataService', ['$http', function ($http) {
+  var controllerBase = "/umbraco/backoffice/KobenIPRestrictor/IpRestrictor/";
+  var loadData = function () {
+    return $http.get(controllerBase + "loadData");
+  };
+
+  var saveData = function (data) {
+    return $http.post(controllerBase + "saveData", JSON.stringify(data));
+  };
+
+  return {
+    loadData: loadData,
+    saveData: saveData
+  }
+}])
