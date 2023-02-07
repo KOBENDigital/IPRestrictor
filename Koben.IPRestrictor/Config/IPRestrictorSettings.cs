12fa80e735edc5468c6beb926b03241ffabba4e5
JontySchmidt <jonty@koben.com.au>
2023-02-07 11:03:35 +1000

    Allow the data-database connection-string name to be changed (eg 'auDataDbDsn')

--- Koben.IPRestrictor/Config/IPRestrictorSettings.cs
+++ Koben.IPRestrictor/Config/IPRestrictorSettings.cs
@@ -8,10 +8,12 @@
 		internal const string StaticUmbracoPath = "/umbraco";
 		internal const string StaticRedirectUrl = "/error-404";
 		internal const bool StaticLogEnabled = false;
+		public const string StaticDataDbDsnName = "dataDbDsn";
 
 		public bool Enabled { get; set; } = StaticEnabled;
 		public string UmbracoPath { get; set; } = StaticUmbracoPath;
 		public string RedirectUrl { get; set; } = StaticRedirectUrl;
 		public bool LogEnabled { get; set; } = StaticLogEnabled;
+		public string DataDbDSNName { get; set; } = StaticDataDbDsnName;
 	}
 }
--- Koben.IPRestrictor/Startup/CoreComposer.cs
+++ Koben.IPRestrictor/Startup/CoreComposer.cs
@@ -1,13 +1,13 @@
-﻿using Koben.IPRestrictor.Extensions;
+﻿using Koben.IPRestrictor.Config;
+using Koben.IPRestrictor.Extensions;
 using Koben.IPRestrictor.Models;
-using Koben.IPRestrictor.Services.IpDataService.Interfaces;
 using Koben.IPRestrictor.Services.IpDataService;
+using Koben.IPRestrictor.Services.IpDataService.Interfaces;
 using Koben.IPRestrictor.Services.IpDataService.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Core.Services;
 
 namespace Koben.IPRestrictor.Startup
 {
@@ -15,8 +15,20 @@ namespace Koben.IPRestrictor.Startup
 	{
 		public void Compose(IUmbracoBuilder builder)
 		{
+			var config = builder.Config;
+
 			builder.Services.AddSingleton<Koben.Persistence.Interfaces.IDataModelMapper<WhitelistedIpPoco, WhitelistedIpDto>, WhitelistedIpMapper>();
-			builder.Services.AddSingleton<Koben.Persistence.Interfaces.IDatabaseProvider>(x => new Koben.Persistence.NPoco.Persistence.SqlServerDatabaseProvider(builder.Config.GetConnectionString("dataDbDSN")));
+			builder.Services.AddSingleton<Koben.Persistence.Interfaces.IDatabaseProvider>
+			(x => 
+				new Koben.Persistence.NPoco.Persistence.SqlServerDatabaseProvider
+				(
+					config.GetConnectionString
+					(
+						config.GetSection(IPRestrictorSettings.IPRestrictorSection)
+						.GetValue(nameof(IPRestrictorSettings.DataDbDSNName), IPRestrictorSettings.StaticDataDbDsnName)
+					)
+				)
+			);
 			builder.Services.AddSingleton<IWhitelistedIpDataService, WhitelistedIpDataService>();
 			builder.AddIPRestrictorConfigs();
 		}
